fc92f2e5b50a281226d21ad5594f330897cc1e55
michbart <bartosmichal2@gmail.com>
2021-11-07 12:26:24 +0100

    Make Material module work.

--- cafeteria-enduser/src/app/layout/layout.module.ts
+++ cafeteria-enduser/src/app/layout/layout.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from '../material.module';
 import { FooterComponent } from './footer/footer.component';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
@@ -7,6 +8,7 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
 @NgModule({
   imports: [
     RouterModule,
+    MaterialModule,
   ],
   declarations: [
     ToolbarComponent,
