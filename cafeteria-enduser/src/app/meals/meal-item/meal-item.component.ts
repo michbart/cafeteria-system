aff4a9c8532e78ce392065a1a2a3147d81301bc8
Michal Bartos <bartosmichal2@gmail.com>
2021-12-04 22:56:52 +0100

    Display order button only for authenticated user.

--- cafeteria-enduser/src/app/meals/meal-item/meal-item.component.ts
+++ cafeteria-enduser/src/app/meals/meal-item/meal-item.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { SecurityProvider } from 'src/app/auth/security-provider';
 import { Meal } from '../meal';
 
 @Component({
@@ -9,5 +10,11 @@ import { Meal } from '../meal';
 export class MealItemComponent {
 
   @Input() public meal!: Meal;
+
+  constructor(private securityProvider: SecurityProvider) { }
+
+  get displayButton() {
+    return this.securityProvider.isAuthenticated();
+  }
 
 }
